9e6bc7ea19a8d38be1fb0a3f768be7000c993b8f
Rae J. Khan <raekhan18@gmail.com>
2021-09-16 00:04:31 +0000

    Select exercises

---
+++ 1_queries/propertyListingsByCity.sql
@@ -0,0 +1,9 @@
+SELECT properties.*, properties.title, properties.cost_per_night, AVG(property_reviews.rating) AS average_rating
+FROM properties
+JOIN property_reviews
+ON properties.id = property_reviews.property_id
+WHERE city = 'Vancouver'
+GROUP BY properties.id
+HAVING AVG(property_reviews.rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
